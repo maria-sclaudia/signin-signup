80cb1042f10362bcc3f2c670c4631bd7b9626943
Maria Cláudia Souza <53797829+maria-sclaudia@users.noreply.github.com>
2021-08-08 11:58:12 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import './config/env';
 import 'reflect-metadata';
 import express, { request, response } from 'express';
+import cors from 'cors';
 
 import './database';
 import { router } from './routes/routes';
@@ -8,6 +9,7 @@ import { router } from './routes/routes';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 
